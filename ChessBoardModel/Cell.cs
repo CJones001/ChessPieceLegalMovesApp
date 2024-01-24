3a11ec6db657ecec84ab1011e1e28e1c05c56c96
CJones001 <147557670+CJones001@users.noreply.github.com>
2024-01-24 08:49:07 -0700

    Add project files.

---
+++ ChessBoardModel/Cell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardModel
+{
+    public class Cell
+    {
+        // Row and Column for the Cells location on the grdi
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+
+        // T/F; is the chess piece on this cell?
+        public bool CurrentlyOccupied { get; set; }
+
+        // T/F; is this cell legal for the chess piece to move to?
+        public bool LegalNextMove { get; set; }
+
+        // Constructor
+        public Cell(int r, int c)
+        {
+            RowNumber = r;
+            ColumnNumber = c;
+        }
+    }
+}
